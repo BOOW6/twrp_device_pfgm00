93fe3b88c387fcd3fc508977f5748643baa99993
BOOW6 <468385005@qq.com>
2023-08-20 09:38:36 +0800

    -

--- twrp_oppo6833.mk
+++ twrp_oppo6833.mk
@@ -7,7 +7,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
 # Inherit from TWRP common configurations
 $(call inherit-product, vendor/twrp/config/common.mk)
 # enable v-ab ota
@@ -20,14 +19,14 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery \
-    libion.recovery
-#     android.hardware.boot@1.0-impl \
-#     android.hardware.boot@1.0-service \
-#     update_engine \
-#     update_verifier \
-#     fsck.f2fs \
-#     mkfs.f2fs \
-#     otapreopt_script
+    libion.recovery \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    update_engine \
+    update_verifier \
+    fsck.f2fs \
+    mkfs.f2fs \
+    otapreopt_script
 
 # PRODUCT_PACKAGES_DEBUG += \
 #     bootctl
@@ -47,11 +46,11 @@ PRODUCT_MANUFACTURER := alps
 
 # Hack: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
-#    ro.crypto.volume.metadata.method=dm-default-key \
-#    ro.crypto.dm_default_key.options_format.version=2 \
-#    ro.crypto.volume.options=::v2 \
-#    ro.vendor.mtk_f2fs_enable=1
+    ro.vendor.build.security_patch=2099-12-31
+#     ro.crypto.volume.metadata.method=dm-default-key \
+#     ro.crypto.dm_default_key.options_format.version=2 \
+#     ro.crypto.volume.options=::v2 \
+#     ro.vendor.mtk_f2fs_enable=1
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
     ro.product.device \
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo twrp_oppo6833-eng
