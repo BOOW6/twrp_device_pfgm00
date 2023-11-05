8e819906071ec26e9ae6bc5a004ff0724336ca35
BOOW6 <468385005@qq.com>
2023-11-05 11:22:42 +0800

    -

--- twrp_oppo6833.mk
+++ twrp_oppo6833.mk
@@ -17,43 +17,49 @@ $(call inherit-product, device/alps/oppo6833/device.mk)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # Extra required packages
+# Boot control HAL
+# Health HAL
+# Storage health HAL
+# A/B support
+# Write flags to the vendor space in /misc partition
 PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0 \
     android.hardware.fastboot@1.0-impl-mtk \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery \
-    android.hardware.boot@1.0 \
-    android.hardware.boot@1.1 \
-    android.hardware.health@1.0 \
-    android.hardware.health@2.0 \
-    android.hardware.health@2.0-impl-default \
-    android.hardware.health@2.0-impl-2.1 \
-    android.hardware.health@2.0-service \
-    android.hardware.health@2.1 \
-    android.hardware.health@2.1-service \
+    \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
+    \
+    android.hardware.health@2.0-service.crosshatch \
+    \
+    android.hardware.health.storage@1.0-service \
     android.hardware.power.stats@1.0-service \
-    libion.recovery \
+    \
+    otapreopt_script \
+    cppreopts.sh \
     update_engine \
     update_verifier \
-    fsck.f2fs \
-    mkfs.f2fs \
-    otapreopt_script \
-    update_engine_sideload \
+    \
     misc_writer \
+    \
+    update_engine_sideload \
     libmalistener
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctl \
     sg_write_buffer \
     f2fs_io \
     check_f2fs \
+    bootctl \
     update_engine_client
 
-# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#     libcutils \
-#     libgptutils \
-#     libz
+# PRODUCT_STATIC_BOOT_CONTROL_HAL is obsolete. Use shared library module instead.
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#    libcutils \
+#    libgptutils \
+#    libz
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := oppo6833
@@ -65,7 +71,8 @@ PRODUCT_MANUFACTURER := alps
 
 # Hack: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.crypto.volume.filenames_mode=aes-256-cts
 
 # PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
 #     ro.product.device \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_oppo6833-user
+add_lunch_combo twrp_oppo6833-userdebug
 add_lunch_combo twrp_oppo6833-eng
